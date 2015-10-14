50d1ce95d27c242c85e920749432c613122f6194
Niranjan Prithviraj <niranjan.prithviraj@gmail.com>
2015-10-13 23:13:59 -0700

    adding some print logs

--- src/cli_tools/core.clj
+++ src/cli_tools/core.clj
@@ -5,17 +5,13 @@
 (declare get-line-count)
 (declare split-files)
 (declare find-file-pos)
-(declare parse-int)
 
 (defn -main [filepath numberoffilestosplitinto]
-  (println filepath)
-  (println numberoffilestosplitinto)
-  (def numberoffilestosplitinto (parse-int numberoffilestosplitinto))
-  (println (read-string numberoffilestosplitinto))
-  (def linecount (get-line-count filepath))
-  (split-files linecount filepath numberoffilestosplitinto)
-  (println linecount)
-  )
+	(println filepath)
+	(println numberoffilestosplitinto)
+	(def linecount (get-line-count filepath))
+	(split-files linecount filepath numberoffilestosplitinto)
+)
 
 (defn get-line-count [filepath]
 	(def linecount 0)
@@ -23,7 +19,7 @@
    	(doseq [l (line-seq r)]
     (def linecount (+ 1 linecount))))
 	linecount
-	)
+)
 
 (defn split-files [linecount filepath numberoffilestosplitinto]
 	;(def destfilepath "/tmp/file-1.txt")
@@ -34,21 +30,16 @@
    		(let [currpos (find-file-pos linecount currlinenumber)
 			numberoffilestosplitinto (read-string numberoffilestosplitinto)
    			fileext (mod currpos numberoffilestosplitinto)]
-   			(println (string/join " " [fileext "   " currlinenumber]))
-   			(with-open [wrtr (clojure.java.io/writer "/tmp/test1.txt" :append true)]
-	  		(.write wrtr l)
-	  		(.write wrtr "\n"))
-		)
-
-	    
+   			(println (string/join "    " [currpos fileext currlinenumber (string/join "" ["/tmp/file-" fileext ".txt"])]))
+   			;(with-open [wrtr (clojure.java.io/writer (string/join "" ["/tmp/file-" fileext ".txt"]) :append true)]
+	  		;(.write wrtr l)
+	  		;(.write wrtr "\n"))
+		)	    
    		(def currlinenumber (+ 1 currlinenumber))
     ))
-	)
+)
 
 (defn find-file-pos [totallines currline]
 	(def currpos (int (Math/floor(* (/ currline totallines) 100))))
 	currpos
 	)
-
-(defn parse-int [s]
-   (Integer. (re-find  #"\d+" s )))
